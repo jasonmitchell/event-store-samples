c474316ee2a8714d4770a529a2965c636e7c2f59
Jason Mitchell <jasonmitchell@live.co.uk>
2017-03-31 23:22:07 +0100

    Removing Given methods

--- Aggregates/Sales/Order.cs
+++ Aggregates/Sales/Order.cs
@@ -7,11 +7,21 @@ namespace Aggregates.Sales
 {
     public class Order : Aggregate
     {
+        public ReadOnlyCollection<OrderItem> Items { get; private set; }
+        public bool Paid { get; private set; }
+        public bool Delivered { get; private set; }
+        public decimal TotalValue => Items.Sum(x => x.Price);
+
         private Order()
         {
-            Given<OrderPlaced>(Given);
-            Given<PaymentReceived>(Given);
-            Given<OrderDelivered>(Given);
+            Given<OrderPlaced>(e =>
+            {
+                Id = e.Id;
+                Items = new ReadOnlyCollection<OrderItem>(e.Items);
+            });
+
+            Given<PaymentReceived>(e => Paid = true);
+            Given<OrderDelivered>(e => Delivered = true);
         }
 
         public Order(Guid id, List<OrderItem> items) : this()
@@ -19,35 +29,14 @@ namespace Aggregates.Sales
             Then(new OrderPlaced(id, items));
         }
 
-        public ReadOnlyCollection<OrderItem> Items { get; private set; }
-        public bool Paid { get; private set; }
-        public bool Delivered { get; private set; }
-        public decimal TotalValue => Items.Sum(x => x.Price);
-
-        private void Given(OrderPlaced e)
-        {
-            Id = e.Id;
-            Items = new ReadOnlyCollection<OrderItem>(e.Items);
-        }
-
         public void Pay()
         {
             Then(new PaymentReceived(Id, TotalValue));
         }
 
-        private void Given(PaymentReceived e)
-        {
-            Paid = true;
-        }
-
         public void DeliveredToRecipient()
         {
             Then(new OrderDelivered(Id));
-        }
-
-        private void Given(OrderDelivered e)
-        {
-            Delivered = true;
         }
     }
 }
